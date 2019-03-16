562c1278a586fbbb4969955c323d98b0b871e212
zhangyongwang <20225614@qq.com>
2019-03-16 20:46:56 +0800

    tab 组件抽象和封装

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <v-header :seller="seller"></v-header>
     <div class="tab-wrapper">
-      <tab></tab>
+      <tab :tabs="tabs"></tab>
     </div>
   </div>
 </template>
@@ -12,7 +12,9 @@
 import VHeader from "components/v-header/v-header";
 import Tab from "components/tab/tab";
 import { getSeller } from "api";
-
+import Goods from "components/goods/goods";
+import Ratings from "components/ratings/ratings";
+import Seller from "components/seller/seller";
 
 export default {
   name: "app",
@@ -20,6 +22,15 @@ export default {
     return {
       seller: {}
     };
+  },
+  computed: {
+    tabs() {
+      return [
+        { label: "商品", component: Goods, data: { seller: this.seller } },
+        { label: "评论", component: Ratings, data: { seller: this.seller } },
+        { label: "商家", component: Seller, data: { seller: this.seller } }
+      ];
+    }
   },
   created() {
     this._getSeller();
