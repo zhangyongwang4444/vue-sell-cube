ca78a6250ef856e801554bf96a294b950e8ebdab
zhangyongwang <20225614@qq.com>
2019-03-16 14:22:22 +0800

    完成header组件

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-header :seller="seller"></v-header>
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VHeader from "components/v-header/v-header";
+import { getSeller } from "api";
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      seller: {}
+    };
+  },
+  created() {
+    this._getSeller();
+  },
+  methods: {
+    _getSeller() {
+      getSeller().then(seller => {
+        this.seller = seller;
+      });
+    }
+  },
   components: {
-    HelloWorld
+    VHeader
   }
-}
+};
 </script>
+
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
 </style>
+
---
+++ src/register.js
@@ -0,0 +1,9 @@
+import { createAPI } from 'cube-ui'
+import Vue from 'vue'
+
+
+import HeaderDetail from 'components/header-detail/header-detail'
+
+createAPI(Vue,HeaderDetail)
+
+
