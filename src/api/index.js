595462a680967af7961b36c16273fcdfee36ea1b
zhangyongwang <20225614@qq.com>
2019-03-21 11:44:39 +0800

    侧边栏内容定制化

--- src/api/index.js
+++ src/api/index.js
@@ -9,3 +9,4 @@ export{
     getGoods,
     getRatings
 }
+ 
--- src/components/tab/tab.vue
+++ src/components/tab/tab.vue
@@ -20,7 +20,7 @@
         @scroll="onScroll"
       >
         <cube-slide-item v-for="(tab,index) in tabs" :key="index">
-          <component :is="tab.component" :data="tab.data"></component>
+          <component :is="tab.component" :data="tab.data" ref="component"></component>
         </cube-slide-item>
       </cube-slide>
     </div>
@@ -34,7 +34,7 @@ export default {
     tabs: {
       type: Array,
       default() {
-        return {};
+        return [];
       }
     },
     initialIndex: {
@@ -64,9 +64,14 @@ export default {
       }
     }
   },
+  mounted() {
+    this.onChange(this.index);
+  },
   methods: {
     onChange(current) {
       this.index = current;
+      const component = this.$refs.component[current];
+      component.fetch && component.fetch();
     },
     onScroll(pos) {
       const tabBarWidth = this.$refs.tabBar.$el.clientWidth;
